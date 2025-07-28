4eb49d77a38fe8aad78a844bea80f594c4b7ac7d
Vijay Bavoor <vijaybavoor.it@gmail.com>
2025-07-29 00:08:46 +0530

    Update ProductCreatedConsumer to handle product creation events
    - Update launchSettings.json for better debugging
    - Update Program.cs to include new consumer

--- Infrastructuer/Consumer/ProductCreatedConsumer.cs
+++ Infrastructuer/Consumer/ProductCreatedConsumer.cs
@@ -1,12 +1,17 @@
 using MassTransit;
 using UseOfMastransistForRabbitMQ.Models;
 
-public class ProductCreatedConsumer : IConsumer<Product>, IProductCreatedConsumer
+public class ProductCreatedConsumer(ILogger<ProductCreatedConsumer> logger) : IConsumer<Product>, IProductCreatedConsumer
 {
     public async Task Consume(ConsumeContext<Product> context)
     {
         var product = context.Message;
-        Console.WriteLine($"Product Received: {product.Name} (ID: {product.Id}) - Price: ₹{product.Price}");
+        if (product == null)
+        {
+            logger.LogError("Received null product message.");
+            return;
+        }
+        logger.LogInformation($"Product Created: {product.Name} (ID: {product.Id}) - Price: ₹{product.Price}");
 
         // You can add further logic here, e.g., storing in a database
         await Task.CompletedTask;
--- Program.cs
+++ Program.cs
@@ -39,6 +39,6 @@ app.UseHttpsRedirection();
 
 //app.UseAuthorization();
 
-//app.MapControllers();
+app.MapControllers();
 
 app.Run();
